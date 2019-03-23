1e9c099171dfe6609965979e766ef81d5d5c7a16
1862386 <league@iMac-2.attlocal.net>
2019-03-23 10:46:41 -0700

    transitions

--- src/body_part_quiz/BodyPartQuiz.java
+++ src/body_part_quiz/BodyPartQuiz.java
@@ -51,23 +51,32 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+int Quiz = 0;
 		// 2. Set the size of the window in the initializeGui() method 
-
-		// 4. Ask the user who this person is and store their answer
+initializeGui();
+	for (int i = 0; i < 4; i++) {
+		
+	
+// 4. Ask the user who this person is and store their answer
 		String guess = JOptionPane.showInputDialog("who is this?");
 
 		// 5. Check their answer. If they guessed correctly:
 		// -- Tell them they are right and increase the score by 1
-
+if (i=1)
+		if(guess .equals("Arnold")) {
+	JOptionPane.showMessageDialog(null, "Correct!");
+	Quiz = Quiz +1;
+}
 		// 6. Otherwise:
 		// -- Tell them they are wrong and who the person is
-
+else{
+	JOptionPane.showMessageDialog(null, "You are wrong. It is" + );
+}
 		// 7. Use the showNextImage() method below to get the next image
 		showNextImage();
-
+	}
 		// 8. .... repeat 4-7 for all your images.....
-
+//(line 57)
 		// 9. Show them their current score
 
 	}
---
+++ src/houses/Houses.java
@@ -0,0 +1,37 @@
+package houses;
+
+public class Houses {
+
+	
+	
+	
+	for(int i=0; i < 4; i++) {
+	
+		println(i);
+		
+	}
+
+	private void println(int i2) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
